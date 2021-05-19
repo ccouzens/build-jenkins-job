bc0d9f3b46e2f8f9e41c0c5940459e771730d4b4
Chris Couzens <chris.couzens@loveholidays.com>
2021-05-19 11:38:52 +0100

    Remove queue dependent code
    The queue was coming up empty

--- entrypoint.py
+++ entrypoint.py
@@ -33,41 +33,5 @@ print(f"Hello {user['fullName']} from Jenkins {version}")
 # build job
 split = JOB_PATH.split("job/")
 job_name = "".join(split)
-print(json.loads(JOB_PARAMS))
-print(job_name)
 
 server.build_job(job_name, parameters=json.loads(JOB_PARAMS), token=JENKINS_TOKEN)
-queue_info = server.get_queue_info()
-print(queue_info)
-queue_id = queue_info[0].get('id')
-
-# define url to request build_number
-url = f"https://{JENKINS_USER}:{JENKINS_TOKEN}@{JENKINS_URL}/queue/item/{queue_id}/api/json?pretty=true"
-
-
-def get_trigger_info(url: str):
-    trigger_info = requests.get(url).json()
-    return trigger_info
-
-
-while "executable" not in (info := get_trigger_info(url)):
-    time.sleep(3)
-
-build_number = info["executable"]["number"]
-print(f"BUILD NUMBER: {build_number}")
-
-
-def get_status(name: str, number: int) -> str:
-    build_info = server.get_build_info(name=name, number=number)
-    job_status = build_info["result"]
-    return job_status
-
-
-while not (status := get_status(job_name, build_number)):
-    time.sleep(1)
-
-print(f"Job status is : {status}")
-print(f"::set-output name=job_status::{status}")
-
-if status != 'SUCCESS':
-    exit(1)
